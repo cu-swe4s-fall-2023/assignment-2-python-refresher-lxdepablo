add9059566f41cf5934a200e140eab345c8f430d
Luis X. de Pablo <luisxdepablo@gmail.com>
2023-09-08 21:07:27 +0000

    wrote get_column function

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,19 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    # make an empty list to store results
+    result_values = []
+
+    # open the file and process it line by line
+    with open(file_name, 'r') as file:
+        for line in file:
+            # split the line into an array using a comma as the delimiter
+            parts = line.strip().split(',')
+
+            # check if the value at the current position in query_column matches the query_value
+            if len(parts) > query_column and parts[query_column] == query_value:
+                # check if the result_column position exists in the array
+                if len(parts) > result_column:
+                    # add the value in the result_column position to the result_values list
+                    result_values.append(parts[result_column])
+
+    # Return the list of result values
+    return result_values
