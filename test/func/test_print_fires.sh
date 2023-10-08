0e9926e1c51abe99882f2a909fb4338fad75e68e
Luis X. de Pablo <luisxdepablo@gmail.com>
2023-10-08 22:31:27 +0000

    fixed filepaths in func test

--- test/func/test_print_fires.sh
+++ test/func/test_print_fires.sh
@@ -3,14 +3,14 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
-run test_mean python ../../src/print_fires.py -a Samoa -b 0 -c 3 -d "/data/Agrofood_co2_emission.csv" -e mean
+run test_mean python ../../src/print_fires.py -a Samoa -b 0 -c 3 -d "../../data/Agrofood_co2_emission.csv" -e mean
 assert_in_stdout 0.5161290322580645
 assert_exit_code 0
 
-run test_median python ../../src/print_fires.py -a Samoa -b 0 -c 3 -d "/data/Agrofood_co2_emission.csv" -e median
+run test_median python ../../src/print_fires.py -a Samoa -b 0 -c 3 -d "../../data/Agrofood_co2_emission.csv" -e median
 assert_in_stdout 0
 assert_exit_code 0
 
-run test_stddev python ../../src/print_fires.py -a Samoa -b 0 -c 3 -d "/data/Agrofood_co2_emission.csv" -e standard_deviation
+run test_stddev python ../../src/print_fires.py -a Samoa -b 0 -c 3 -d "../../data/Agrofood_co2_emission.csv" -e standard_deviation
 assert_in_stdout 1.3166665569005518
 assert_exit_code 0
