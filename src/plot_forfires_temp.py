2ab36a4866fe9683da7d976d046848e3e6cbd261
Luis X. de Pablo <luisxdepablo@gmail.com>
2023-10-19 17:58:48 +0000

    updated plot code to pass linter

--- src/plot_forfires_temp.py
+++ src/plot_forfires_temp.py
@@ -1,19 +1,19 @@
 import sys
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import my_utils
+matplotlib.use('Agg')
 
 data_file = sys.argv[1]
 out_file = sys.argv[2]
 country = sys.argv[3]
 title = sys.argv[4]
-x=sys.argv[5]
-y=sys.argv[6]
+x = sys.argv[5]
+y = sys.argv[6]
 
-forest_fires = my_utils.get_column(data_file, 0, country, result_column = 3)
-avg_temps = my_utils.get_column(data_file, 0, country, result_column = 30)
+forest_fires = my_utils.get_column(data_file, 0, country, result_column=3)
+avg_temps = my_utils.get_column(data_file, 0, country, result_column=30)
 
 fig, ax = plt.subplots()
 ax.scatter(avg_temps, forest_fires)
@@ -23,4 +23,4 @@ ax.set_xlabel(x)
 ax.set_ylabel(y)
 ax.set_title(title)
 
-plt.savefig(out_file,bbox_inches='tight')
+plt.savefig(out_file, bbox_inches='tight')
--- src/plot_temp_year.py
+++ src/plot_temp_year.py
@@ -1,19 +1,19 @@
 import sys
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import my_utils
+matplotlib.use('Agg')
 
 data_file = sys.argv[1]
 out_file = sys.argv[2]
 country = sys.argv[3]
 title = sys.argv[4]
-x=sys.argv[5]
-y=sys.argv[6]
+x = sys.argv[5]
+y = sys.argv[6]
 
-avg_temps = my_utils.get_column(data_file, 0, country, result_column = 30)
-years = my_utils.get_column(data_file, 0, country, result_column = 1)
+avg_temps = my_utils.get_column(data_file, 0, country, result_column=30)
+years = my_utils.get_column(data_file, 0, country, result_column=1)
 
 fig, ax = plt.subplots()
 ax.scatter(years, avg_temps)
@@ -23,4 +23,4 @@ ax.set_xlabel(x)
 ax.set_ylabel(y)
 ax.set_title(title)
 
-plt.savefig(out_file,bbox_inches='tight')
+plt.savefig(out_file, bbox_inches='tight')
